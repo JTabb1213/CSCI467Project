72ea4aa669a36c3acbef97bb8663e7d594561471
Jack Tabb <jacktabb@Jacks-MacBook-Pro-3.local>
2024-11-12 13:51:38 -0600

    Added database to hold the sales quotes, and also partially implemented the part where the sales associate adds/edits them

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS customer_quotes (
+    associateID VARCHAR(255) NOT NULL,   
+    custID INT NOT NULL,                  
+    price DECIMAL(10, 2) NOT NULL,       
+    email VARCHAR(255),                   
+    description TEXT,                    
+    secretNotes TEXT,                    
+    PRIMARY KEY (custID)                  
+);
--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,37 +1,36 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-import random
+import random, traceback
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})# Allow access to all routes(r"/*") from origin localhost
+
+#External database endpoints in this form: dbtype://user:password@host:port/db1
+engine1 = create_engine("mysql+pymysql://student:student@blitz.cs.niu.edu:3306/csci467")
+engine2 = create_engine("mysql+pymysql://sql5743974:7h7d7IQbUh@sql5.freesqldatabase.com:3306/sql5743974")
+Base = declarative_base()
+#establish session connection
+SessionLocal1 = sessionmaker(autocommit=False, autoflush=False, bind=engine1)
+SessionLocal2 = sessionmaker(autocommit=False, autoflush=False, bind=engine2)
 
 #to generate the random number
 def generate_random_string():
     random_number = random.randint(100000, 999999)
     return f"xyz-{random_number}-cba"
 
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})# Allow access to all routes(r"/*") from origin localhost
-
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mysql+pymysql://student:student@blitz.cs.niu.edu:3306/csci467'
-)
-
-app.config['ORDER_DATABASE_URL'] = {
-    'http://blitz.cs.niu.edu/PurchaseOrder/'
-}
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Track modifications made to DB
-
-db = SQLAlchemy(app)
-
 #Model for the DB
-class YourModel(db.Model):
+class Customer(Base):
     __tablename__ = 'customers'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    city = db.Column(db.String(50))
-    street = db.Column(db.String(50))
-    contact = db.Column(db.String(50))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    city = Column(String(50))
+    street = Column(String(50))
+    contact = Column(String(50))
+
 
     def to_dict(self):  # Return the data from the DB in a dict form
         return {
@@ -48,11 +47,33 @@ def get_message():
 
 @app.route('/all_customer_info', methods=['GET']) # Call this route from  the frontend to get all customers
 def get_all_customer_info():
-    customers = YourModel.query.all()
-    return jsonify([customer.to_dict() for customer in customers]) 
 
-@app.route('/purchase_order', methods=['POST'])
+    session = SessionLocal1() #Use session 1
+    try:
+        customers = session.query(Customer).all()
+        return jsonify([customer.to_dict() for customer in customers])
+    except Exception as e:
+        error_message = traceback.format_exc()
+        return jsonify(error=error_message), 500
+    finally:
+        session.close()  #Close the session
+
+@app.route('/test_db_connection', methods=['GET'])#This is just used to test if connection to a database is successful
+def test_db_connection():
+    session = SessionLocal2() #test session 2, 
+    try:
+        result = session.execute(text('SELECT 1'))
+        result.fetchall()
+        return jsonify(message="Connection successful to the second database!")
+    except Exception as e:
+        #error_message = traceback.format_exc()
+        return jsonify(str(e)), 500
+    finally:
+        session.close()  # Close the session
+        
+@app.route('/purchase_order', methods=['POST'])#Endpoint used by sales associate to add/update a quote to our database
 def send_purchase_order():
+    session = SessionLocal2() #use session 2
     try:
         data = request.get_json()
         order = generate_random_string()
@@ -62,9 +83,25 @@ def send_purchase_order():
         email = data['email']
         description = data['description']
         secrets = data['secretNotes']
-        return jsonify({'data recieved' : data}), 200
+        session.execute(text("""
+            INSERT INTO customer_quotes (associateID, custID, price, email, description, secretNotes)
+            VALUES (:associate, :custid, :price, :email, :description, :secrets)
+        """), {
+            'associate': associate,
+            'custid': custid,
+            'price': price,
+            'email': email,
+            'description': description,
+            'secrets': secrets
+        })
+
+        session.commit()
+
+        return jsonify({'data recieved, and added to database' : data}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+    finally:
+        session.close() #close the session
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
