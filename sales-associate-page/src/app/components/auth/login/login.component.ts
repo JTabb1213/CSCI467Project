b5aa2a3275af46154e74a2bbdd6445688d3b1fdd
Fluffy Hydra <mikejosephsanders@gmail.com>
2024-11-21 04:29:25 -0600

    Add fourms for the user to put in an username and password. Currently will suceed no matter what is entered.

--- sales-associate-page/src/app/components/auth/login/login.component.ts
+++ sales-associate-page/src/app/components/auth/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // In order to get the login boxes to work 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // need to add it here too in order to enable it
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onLogin() { // Logs the username and password on the console (Can be seen in chrome deveoper tools)
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+
+    // Goes straight to successful login
+    // Currently doesn't check, sucess as long as you enter anything
+    alert('Login Successful!');
+  }
 }
